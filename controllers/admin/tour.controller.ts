0e2c7ac3ef744c624e52b0c187589812b5d77ee1
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-11-24 15:05:03 +0700

    feat: tour edit api

--- controllers/admin/tour.controller.ts
+++ controllers/admin/tour.controller.ts
@@ -183,6 +183,138 @@ export const list = async (req: AccountRequest, res: Response) => {
   }
 };
 
+export const edit = async (req: AccountRequest, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const tourDetail = await Tour.findOne({
+      _id: id,
+      deleted: false,
+    });
+
+    const dataFinal = {
+      id: tourDetail?.id,
+      name: tourDetail?.name,
+      category: tourDetail?.category,
+      position: tourDetail?.position,
+      status: tourDetail?.status,
+      avatar: tourDetail?.avatar,
+      priceAdult: tourDetail?.priceAdult,
+      priceChildren: tourDetail?.priceChildren,
+      priceBaby: tourDetail?.priceBaby,
+      priceNewAdult: tourDetail?.priceNewAdult,
+      priceNewChildren: tourDetail?.priceNewChildren,
+      priceNewBaby: tourDetail?.priceNewBaby,
+      stockAdult: tourDetail?.stockAdult,
+      stockChildren: tourDetail?.stockChildren,
+      stockBaby: tourDetail?.stockBaby,
+      information: tourDetail?.information,
+      locationsFrom: tourDetail?.locationsFrom,
+      locationsTo: tourDetail?.locationsTo,
+      time: tourDetail?.time,
+      vehicle: tourDetail?.vehicle,
+      schedules: tourDetail?.schedules,
+      departureDateFormat: "",
+    };
+
+    if (tourDetail?.departureDate) {
+      dataFinal.departureDateFormat = moment(tourDetail.departureDate).format(
+        "YYYY-MM-DD"
+      );
+    }
+
+    res.status(200).json({
+      message: "Chi tiết tour!",
+      tourDetail: dataFinal,
+    });
+  } catch (error) {
+    console.log("Lỗi khi gọi edit", error);
+    res.status(500).json({ message: "Lỗi hệ thống!" });
+  }
+};
+
+export const editPatch = async (req: AccountRequest, res: Response) => {
+  try {
+    const id = req.params.id;
+
+    const tourDetail = await Tour.findOne({
+      _id: id,
+      deleted: false,
+    });
+
+    if (!tourDetail) {
+      return res.status(404).json({ message: "Tour không tồn tại!" });
+    }
+
+    if (req.body.position) {
+      req.body.position = parseInt(req.body.position);
+    } else {
+      const totalRecord = await Tour.countDocuments({});
+      req.body.position = totalRecord + 1;
+    }
+
+    req.body.priceAdult = req.body.priceAdult
+      ? parseInt(req.body.priceAdult)
+      : 0;
+    req.body.priceChildren = req.body.priceChildren
+      ? parseInt(req.body.priceChildren)
+      : 0;
+    req.body.priceBaby = req.body.priceBaby ? parseInt(req.body.priceBaby) : 0;
+
+    req.body.priceNewAdult = req.body.priceNewAdult
+      ? parseInt(req.body.priceNewAdult)
+      : req.body.priceAdult;
+    req.body.priceNewChildren = req.body.priceNewChildren
+      ? parseInt(req.body.priceNewChildren)
+      : req.body.priceChildren;
+    req.body.priceNewBaby = req.body.priceNewBaby
+      ? parseInt(req.body.priceNewBaby)
+      : req.body.priceBaby;
+
+    req.body.stockAdult = req.body.stockAdult
+      ? parseInt(req.body.stockAdult)
+      : 0;
+    req.body.stockChildren = req.body.stockChildren
+      ? parseInt(req.body.stockChildren)
+      : 0;
+    req.body.stockBaby = req.body.stockBaby ? parseInt(req.body.stockBaby) : 0;
+
+    req.body.locationsFrom = req.body.locationsFrom
+      ? JSON.parse(req.body.locationsFrom)
+      : [];
+    req.body.locationsTo = req.body.locationsTo
+      ? JSON.parse(req.body.locationsTo)
+      : [];
+    req.body.departureDate = req.body.departureDate
+      ? new Date(req.body.departureDate)
+      : null;
+    req.body.schedules = req.body.schedules
+      ? JSON.parse(req.body.schedules)
+      : [];
+
+    req.body.slug = slugify(req.body.name, { lower: true });
+    req.body.updatedBy = req.account.id;
+    if (req.file) {
+      req.body.avatar = req.file.path;
+    } else {
+      delete req.body.avatar;
+    }
+
+    await Tour.updateOne(
+      {
+        _id: id,
+        deleted: false,
+      },
+      req.body
+    );
+
+    res.status(200).json({ message: "Cập nhật tour thành công!" });
+  } catch (error) {
+    console.log("Lỗi khi gọi editPatch", error);
+    res.status(500).json({ message: "Lỗi hệ thống!" });
+  }
+};
+
 export const deletePatch = async (req: AccountRequest, res: Response) => {
   try {
     const id = req.params.id;
